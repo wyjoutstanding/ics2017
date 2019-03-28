bcf53343e244599a2d564819956cd6a529916c9a
tracer-ics2017 <tracer@njuics.org>
2019-03-28 13:17:05 +0800

    >  compile 161730319 wuyangjun Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  13:17:05 up  4:13,  4 users,  load average: 0.05, 0.01, 0.00 b30ebe1239e77ab41e632da8441478dad19352d8

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -84,13 +84,13 @@ static int cmd_x(char *args) {
 	char *arg2 = strtok(NULL," ");
 	int n = atoi(arg1);
 	uint32_t vaddr;
-    int tmp;
-	sscanf(arg2,"%x",&tmp);
-	vaddr = (uint32_t)tmp;
+//    int tmp;
+	sscanf(arg2,"%x",&vaddr);
+//	vaddr = (uint32_t)tmp;
 	printf("arg2:%s n:%d vaddr:%u\n",arg2,n,vaddr);
 
 	uint32_t instr = vaddr_read(vaddr,n);
-	printf("0x%8x",instr);	
+	printf("0x%08x",instr);	
 	return 0;
 }
 //指令结构体：名称，描述，函数名
