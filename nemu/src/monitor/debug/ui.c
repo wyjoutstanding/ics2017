036e907aed712a550e4e42250f73e2be66f87005
tracer-ics2017 <tracer@njuics.org>
2019-03-28 13:14:00 +0800

    >  compile 161730319 wuyangjun Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  13:14:00 up  4:10,  4 users,  load average: 0.00, 0.00, 0.00 205416d18594d07abecd5d70f2c9f7634fb54d1a

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -83,7 +83,10 @@ static int cmd_x(char *args) {
 	char *arg1 = strtok(NULL," ");
 	char *arg2 = strtok(NULL," ");
 	int n = atoi(arg1);
-	uint32_t vaddr; sscanf(arg2,"%x",&vaddr);
+	uint32_t vaddr;
+    int tmp;
+	sscanf(arg2,"%x",&tmp);
+	vaddr = (uint32_t)tmp;
 	printf("arg2:%s n:%d vaddr:%8x\n",arg2,n,vaddr);
 
 	uint32_t instr = vaddr_read(vaddr,n);
