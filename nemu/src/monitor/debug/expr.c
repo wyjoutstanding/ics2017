5d8aec4d76cab5ba275ba5b7acf5dd277aeb2ccb
tracer-ics2017 <tracer@njuics.org>
2019-04-10 01:49:09 +0800

    >  compile 161730319 wuyangjun Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  01:49:09 up  3:26,  2 users,  load average: 0.02, 0.04, 0.03 66a75110a81c45f2747ca52d2cbe34ab64e6fee0

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -82,7 +82,7 @@ static bool make_token(char *e) {
 
   while (e[position] != '\0') {
     /* Try all rules one by one. */
-
+    Log("e_pos:%c\n",e[position]);
 		for (i = 0; i < NR_REGEX; i ++) {
      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
         char *substr_start = e + position;
