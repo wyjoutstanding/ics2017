37ff11630a57b8f198f5d53ace3d62f422fa7be3
tracer-ics2017 <tracer@njuics.org>
2019-04-10 00:53:14 +0800

    >  compile 161730319 wuyangjun Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  00:53:14 up  2:30,  2 users,  load average: 0.38, 0.21, 0.09 acd76f186e0e4b86f4a07d0a9419484076330908

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <regex.h>
 
+//token的类型，只要每种类型对应的整型不同即可，其余用ASCII码表示
 enum {
   TK_NOTYPE = 256,//space
  	TK_EQ = 255,//==
@@ -16,7 +17,7 @@ enum {
   /* TODO: Add more token types */
 
 };
-
+//检测不同类型的规则
 static struct rule {
   char *regex;
   int token_type;
@@ -41,14 +42,15 @@ static struct rule {
 	{"\\(", '('},					//brakets is special in regex !!!
 	{"\\)", ')'},
 };
-
+//规则条数
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
-
+//regex.h使用必备
 static regex_t re[NR_REGEX];
 
 /* Rules are used for many times.
  * Therefore we compile them only once before any usage.
  */
+//对所有规则编译并检查
 void init_regex() {
   int i;
   char error_msg[128];
@@ -70,7 +72,7 @@ typedef struct token {
 
 Token tokens[32];
 int nr_token;
-
+//提取并按顺序存储token于tokens
 static bool make_token(char *e) {
   int position = 0;
   int i;
@@ -80,24 +82,33 @@ static bool make_token(char *e) {
 
   while (e[position] != '\0') {
     /* Try all rules one by one. */
-    for (i = 0; i < NR_REGEX; i ++) {
-      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+
+		for (i = 0; i < NR_REGEX; i ++) {
+     if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
         Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
         position += substr_len;
-
+				//取子串
+				char* substr = NULL;
+				strncpy(substr,substr_start,substr_len);
+				substr[substr_len] = '\0';
+				if(substr_len >= 32)Log("substr_len is overflow 32!!in make_token");//以后处理
         /* TODO: Now a new token is recognized with rules[i]. Add codes
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
 
         switch (rules[i].token_type) {
-          default: TODO();
-        }
-
+					case TK_HEX: tokens[nr_token].type = TK_HEX; memset(tokens[nr_token].str,'\0',32); strcpy(tokens[nr_token++].str,substr);break;
+				  case TK_DEC: tokens[nr_token].type = TK_DEC; memset(tokens[nr_token].str,'\0',32); strcpy(tokens[nr_token++].str,substr);break;
+          case TK_REG: tokens[nr_token].type = TK_REG; memset(tokens[nr_token].str,'\0',32); strcpy(tokens[nr_token++].str,substr);break;
+				  case TK_NOTYPE: break;//tokens[nr_token].type = TK_DEC; tokens[nr_token++].str = substr;
+				  case TK_EQ: tokens[nr_token++].type = TK_EQ;// tokens[nr_token++].str = "==\0";break;
+					default:tokens[nr_token++].type = rules[i].token_type;break;
+				}
         break;
       }
     }
