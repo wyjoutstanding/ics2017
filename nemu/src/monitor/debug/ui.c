d72ed8c20034995918713729fe6c35f64d4a4494
tracer-ics2017 <tracer@njuics.org>
2019-04-14 00:00:57 +0800

    >  compile 161730319 wuyangjun Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  00:00:57 up  1:14,  2 users,  load average: 0.00, 0.00, 0.00 e79f3f85fe571394951af24dd434c2dc1a2ffc16

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -88,7 +88,9 @@ static int cmd_x(char *args) {
 	bool success = true;
 	uint32_t vaddr;
 	vaddr=expr(arg2,&success);//利用表达式处理地址
-//	sscanf(arg2,"%x",&vaddr);//16进制字符串转换为32位无符号数,表示地址
+  Log("vaddr1:0x%08x\n",vaddr);
+	sscanf(arg2,"%x",&vaddr);//16进制字符串转换为32位无符号数,表示地址
+	Log("vaddr2:0x%08x\n",vaddr);
 	printf("Address       Dword block   Byte sequence\n");
 	
 	uint32_t instr;//存放地址vaddr中的内容
