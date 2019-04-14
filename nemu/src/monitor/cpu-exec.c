6e06dda16b773399dec4adf4962d79a406668a8f
tracer-ics2017 <tracer@njuics.org>
2019-04-14 16:41:45 +0800

    >  compile 161730319 wuyangjun Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  16:41:45 up  5:41,  3 users,  load average: 0.00, 0.00, 0.00 796723e35ede1ca640c625b46fed1dbe7ba1ba97

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -1,6 +1,6 @@
 #include "nemu.h"
 #include "monitor/monitor.h"
-
+//#include "monitor/watchpoint.h"
 /* The assembly code of instructions executed is only output to the screen
  * when the number of instructions executed is less than this value.
  * This is useful when you use the `si' command.
@@ -11,6 +11,7 @@
 int nemu_state = NEMU_STOP;
 
 void exec_wrapper(bool);
+//WP* scan_watchpoint();
 
 /* Simulate how the CPU works. */
 void cpu_exec(uint64_t n) {
@@ -20,19 +21,30 @@ void cpu_exec(uint64_t n) {
     return;
   }
   nemu_state = NEMU_RUNNING;
-
   bool print_flag = n < MAX_INSTR_TO_PRINT;
 
-  for 
-	  (; n > 0; n --) {
+  for (; n > 0; n --) {
     /*
-	 * Execute one instruction, including instruction fetch,
+  	 * Execute one instruction, including instruction fetch,
      * instruction decode, and the actual execution. */
     exec_wrapper(print_flag);
 
 #ifdef DEBUG
     /* TODO: check watchpoints here. */
-
+//		WP* wp_trigger[32];
+/*		WP* p = scan_watchpoint(wp_trigger),*wp = NULL;
+		if(p != NULL)
+	   	for(int i = 0; i < sizeof(wp_trigger); i++){
+				wp = wp_trigger[i];
+				printf("Hit watchpoint %d at address 0x%08x\n",wp->NO,cpu.eip);
+				printf("%-10s= %s\n","expr",wp->expr);
+				printf("%-10s= %08x\n","old_val",wp->old_val);
+				printf("%-10s= %08x\n","new_val",wp->new_val);
+				printf("program paused\n");
+				wp->old_val = wp->new_val;
+			}
+			if(sizeof(wp_trigger) != 0)nemu_state = NEMU_STOP;
+		}*/
 #endif
 
 #ifdef HAS_IOE
