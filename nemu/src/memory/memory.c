8382a1c0a5415b0accce82953bdccf42ff3efd8f
tracer-ics2017 <tracer@njuics.org>
2019-04-18 20:24:45 +0800

    >  compile 161730319 wuyangjun Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  20:24:45 up  5:46,  3 users,  load average: 0.00, 0.00, 0.00 66183b2b3edf94851ca667d852b0319bc51788

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -13,7 +13,7 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
- printf("pmem u:%p  d:%p u+len:%p",pmem,pmem,pmem+len);
+ printf("pmem u:%p  addr:%p u+len:%p",pmem,(void*)addr,pmem+len);
  return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
 }
 
