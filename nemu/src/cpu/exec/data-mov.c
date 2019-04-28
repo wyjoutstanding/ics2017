907a940e06878f46ec3ce22ef8644518e5f8dff6
tracer-ics2017 <tracer@njuics.org>
2019-04-28 18:45:10 +0800

    >  compile 161730319 wuyangjun Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  18:45:10 up 9 min,  2 users,  load average: 0.00, 0.03, 0.01 5d472b713f9d302eb4b114688728631d7ecd17ff

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -7,7 +7,7 @@ make_EHelper(mov) {
 //push's execute function
 make_EHelper(push) {
  // rtl_push(&reg_l(id_dest->reg));//ebp入栈,esp<-ebp,only consider dword (uint32_t)
-  rtl_push(&id_dest->val);
+  rtl_push(&id_dest->val);//reg/imm/memory
   print_asm_template1(push);
 }
 
