caa93b91f2806cc9910c01085b8dcd0c56711b46
tracer-ics2017 <tracer@njuics.org>
2019-05-07 13:35:44 +0800

    >  compile 161730319 wuyangjun Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  13:35:44 up  1:01,  4 users,  load average: 0.09, 0.02, 0.01 82a0190fc23217da7959ca92c901fa42c8cebebc

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -9,12 +9,25 @@ make_EHelper(add) {
 	exec_adc(eip);//deal with flags
   print_asm_template2(add);
 }
-void exec_sbb(vaddr_t* );
+//void exec_sbb(vaddr_t* );
 make_EHelper(sub) {
 //	printf("1 dest:%u src:%u\n",id_dest->val,id_src->val);
 //  rtl_sub(&id_dest->val,&id_dest->val,&id_src->val);//dest=src-src2
   rtl_sext(&id_src->val,&id_src->val,id_dest->width);
-	exec_sbb(eip);
+	//exec_sbb(eip);sub 与 sbb的区别
+	rtl_sub(&t2,&id_dest->val,&id_src->val);
+	operand_write(id_dest,&t2);
+
+	rtl_update_ZFSF(&t2,id_dest->width);
+
+	rtl_sltu(&t0,&id_dest->val,&t2);
+	rtl_set_CF(&t0);
+
+	rtl_xor(&t0,&id_dest->val,&id_src->val);
+	rtl_xor(&t1,&id_dest->val,&t2);
+	rtl_and(&t0,&t0,&t1);
+	rtl_msb(&t0,&t0,id_dest->width);
+	rtl_set_OF(&t0);
 	print_asm_template2(sub);
 }
 
@@ -67,11 +80,11 @@ make_EHelper(adc) {
 //sub consider OF,CF,ZF,SF
 make_EHelper(sbb) {
   rtl_sub(&t2, &id_dest->val, &id_src->val);
-  operand_write(id_dest,&t2);//test
+  //operand_write(id_dest,&t2);//test
 	rtl_sltu(&t3, &id_dest->val, &t2);
   rtl_get_CF(&t1);
   rtl_sub(&t2, &t2, &t1);
- // operand_write(id_dest, &t2);
+  operand_write(id_dest, &t2);
 
   rtl_update_ZFSF(&t2, id_dest->width);
 
