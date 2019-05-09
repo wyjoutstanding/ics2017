a57b7c2b2968a99f4acb6b137bfde59595bd0e26
tracer-ics2017 <tracer@njuics.org>
2019-05-09 22:31:53 +0800

    >  compile 161730319 wuyangjun Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  22:31:53 up  1:54,  4 users,  load average: 0.06, 0.03, 0.00 fe61cc0d27225fed11a858c283f9721e776f0343

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -50,7 +50,7 @@ make_group(gp1,
 
   /* 0xc0, 0xc1, 0xd0, 0xd1, 0xd2, 0xd3 */
 make_group(gp2,
-    EX(inc), EX(dec), EMPTY, EMPTY,
+    EMPTY, EMPTY, EMPTY, EMPTY,
     EX(shl), EX(shr), EMPTY, EX(sar))
 
   /* 0xf6, 0xf7 */
@@ -60,7 +60,7 @@ make_group(gp3,
 
   /* 0xfe */
 make_group(gp4,
-    EMPTY, EMPTY, EMPTY, EMPTY,
+    EX(inc), EX(dec), EMPTY, EMPTY,
     EMPTY, EMPTY, EMPTY, EMPTY)
 
   /* 0xff */
