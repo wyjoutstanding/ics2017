2089cbf0147a2131e8ea5f56c292dc85df6589ce
tracer-ics2017 <tracer@njuics.org>
2019-05-09 09:45:55 +0800

    >  compile 161730319 wuyangjun Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  09:45:55 up  1:00,  4 users,  load average: 0.12, 0.04, 0.01 2aa8de2ef2c5ad5a6c0f9af7fb7006bcb6c07b12

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -317,3 +317,10 @@ void operand_write(Operand *op, rtlreg_t* src) {
   else if (op->type == OP_TYPE_MEM) { rtl_sm(&op->addr, op->width, src); }
   else { assert(0); }
 }
+//mov Gv <- Eb
+make_DHelper(mov_Eb2Gv) {
+	assert(id_dest->width == 2 || id_dest->width == 4);
+	id_src->width = 1;//binary
+  decode_op_rm(eip,id_src,true,id_dest,false);
+}
+
