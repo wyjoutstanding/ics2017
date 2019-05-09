c732c5fa3a2f413541777c740ba73cf8eda5abe7
tracer-ics2017 <tracer@njuics.org>
2019-05-09 20:45:02 +0800

    >  compile 161730319 wuyangjun Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  20:45:02 up 7 min,  4 users,  load average: 0.14, 0.05, 0.01 a061639f92bf4f52b3a757ed4e2bea8785947726

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -61,16 +61,24 @@ make_EHelper(sar) {
 }
 
 make_EHelper(shl) {
-  TODO();
+//  TODO();
   // unnecessary to update CF and OF in NEMU
+  rtl_shl(&t3,&id_dest->val,&id_src->val);
+	operand_write(id_dest,&t3);
 
+	rtl_update_ZFSF(&t3,id_dest->width);
+  
   print_asm_template2(shl);
 }
 
 make_EHelper(shr) {
   TODO();
   // unnecessary to update CF and OF in NEMU
+  rtl_shr(&t3,&id_dest->val,&id_src->val);
+	operand_write(id_dest,&t3);
 
+	rtl_update_ZFSF(&t3,id_dest->width);
+ 
   print_asm_template2(shr);
 }
 
