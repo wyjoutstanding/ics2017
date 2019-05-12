65b973f1e076d7d7e3a7c46f091068153e3f7143
tracer-ics2017 <tracer@njuics.org>
2019-05-12 17:03:36 +0800

    >  run 161730319 wuyangjun Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  17:03:36 up  2:46,  4 users,  load average: 0.00, 0.00, 0.00 a892b62749fbcfa711fdfea5f21e88e88b0cc6c

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -9,7 +9,9 @@ void _ioe_init() {
 }
 
 unsigned long _uptime() {
-  return 0;
+  unsigned long now_time;
+	now_time = inl(RTC_PORT);
+	return now_time -  boot_time;
 }
 
 uint32_t* const fb = (uint32_t *)0x40000;
