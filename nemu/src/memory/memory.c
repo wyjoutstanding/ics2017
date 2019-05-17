a3f35fcf3b7c1eba66970e2d2bbdbb4536590cb0
tracer-ics2017 <tracer@njuics.org>
2019-05-17 09:13:38 +0800

    >  compile 161730319 wuyangjun Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  09:13:38 up  6:16,  7 users,  load average: 0.07, 0.53, 1.02 d8b3b985e9c4ac4f8e1360aa5d70dd9bb48d25eb

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -2,6 +2,10 @@
 
 #define PMEM_SIZE (128 * 1024 * 1024)
 
+int is_mmio(paddr_t);
+uint32_t mmio_read(paddr_t, int, int);
+void mmio_write(paddr_t, int, uint32_t, int);
+
 #define pmem_rw(addr, type) *(type *)({\
     Assert(addr < PMEM_SIZE, "physical address(0x%08x) is out of bound", addr); \
     guest_to_host(addr); \
@@ -17,11 +21,15 @@ uint32_t paddr_read(paddr_t addr, int len) {
 //  uint32_t *vaddr =(uint32_t*)(pmem + addr);//获取指定指令的地址
 //	*vaddr = *vaddr & 0;
 //	printf("vaddr:%08x\n",*vaddr);
- 	return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  int mmio_id = is_mmio(addr);
+	if(mmio_id != -1)return mmio_read(addr,len,mmio_id) & (~0u >> ((4 - len) << 3));
+	return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
-  memcpy(guest_to_host(addr), &data, len);
+  int mmio_id = is_mmio(addr);
+	if(mmio_id != -1)mmio_write(addr,len,data,mmio_id);
+	else	memcpy(guest_to_host(addr), &data, len);
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
