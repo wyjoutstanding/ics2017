839c1e2d33f08aaf4a116af0643fd6cc092ba9c8
tracer-ics2017 <tracer@njuics.org>
2019-06-06 17:02:21 +0800

    >  run 161730319 wuyangjun Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  17:02:21 up 13 min,  2 users,  load average: 0.10, 0.19, 0.15 a46a971a5c641ede9a5236b97c23db536c0257e

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -10,7 +10,8 @@ static inline uintptr_t sys_open(uintptr_t pathname, uintptr_t flags, uintptr_t 
 }
 static inline uintptr_t sys_write(uintptr_t fd, uintptr_t buf, uintptr_t len) {
 //  TODO();
-  if(fd == 1 || fd == 2){
+  Log("fd:%d len:%d str:%s\n",fd,len,(char*)buf);
+	if(fd == 1 || fd == 2){
 //		char s[len]=vaddr_read(buf,len);
 		for(int i = 0; i < len; i++){
 			_putc(((char*)buf)[i]);
@@ -58,7 +59,7 @@ _RegSet* do_syscall(_RegSet *r) {
   switch (a[0]) {
     case SYS_none: sys_none(r); break;
 		case SYS_exit: sys_exit(); break;
-		case SYS_write: SYSCALL_ARG1(r) = sys_write(a[1], a[2], a[3]); break;
+		case SYS_write: Log("write");SYSCALL_ARG1(r) = sys_write(a[1], a[2], a[3]); break;
 		default: panic("Unhandled syscall ID = %d", a[0]);
   }
   return NULL;
