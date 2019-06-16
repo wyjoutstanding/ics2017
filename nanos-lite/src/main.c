5c9e63020c248cdaf6ed4ee86df9481febbed1c9
tracer-ics2017 <tracer@njuics.org>
2019-06-16 20:43:56 +0800

    >  run 161730319 wuyangjun Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  20:43:56 up 12:23,  3 users,  load average: 0.08, 0.07, 0.06 cc64ec0dbcce251c89e523067f94c0048c0b9d3

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -33,6 +33,6 @@ int main() {
 
 //  uint32_t entry = loader(NULL, "/bin/dummy");
 //  ((void (*)(void))entry)();
-  load_prog("/bin/dummy");
+  load_prog("/bin/hello");
   panic("Should not reach here");
 }
