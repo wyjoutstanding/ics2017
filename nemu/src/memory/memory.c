5eb55357cadb32dae667d611b7770ac113032534
tracer-ics2017 <tracer@njuics.org>
2019-06-16 19:57:59 +0800

    >  compile 161730319 wuyangjun Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  19:57:59 up 11:37,  3 users,  load average: 0.04, 0.03, 0.03 9916292474041b0882376c8ae6da4c97cc6dd037

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -20,64 +20,39 @@ uint8_t pmem[PMEM_SIZE];
 
 uint32_t paddr_read(paddr_t addr ,int len);
 void paddr_write(paddr_t addr, int len, uint32_t data);
-// paddr_t page_translate(vaddr_t addr) {	
-// // 	PDE pd;
-// // 	PTE pb;
+paddr_t page_translate(vaddr_t addr, bool is_write) {	
+// 	PDE pd;
+// 	PTE pb;
 
-// // 	paddr_t pd_addr = (cpu.cr3.page_directory_base << 12) |  (((vaddr >> 22) & 0x3ff) << 2);
-// // 	pd.val = paddr_read(pd_addr, 4);//fetch a PageDirectory
-// // //	Log("cr3:0x%08x cr3<<12:0x%08x vaddr:0x%08x vaddr>>22:0x%08x", cpu.cr3.page_directory_base, cpu.cr3.page_directory_base << 12, vaddr, vaddr >> 22); 
-// // 	assert(pd.present == 1);
+// 	paddr_t pd_addr = (cpu.cr3.page_directory_base << 12) |  (((vaddr >> 22) & 0x3ff) << 2);
+// 	pd.val = paddr_read(pd_addr, 4);//fetch a PageDirectory
+// //	Log("cr3:0x%08x cr3<<12:0x%08x vaddr:0x%08x vaddr>>22:0x%08x", cpu.cr3.page_directory_base, cpu.cr3.page_directory_base << 12, vaddr, vaddr >> 22); 
+// 	assert(pd.present == 1);
 	
-// // 	paddr_t pb_addr = (pd.page_frame << 12) | (((vaddr >> 12) & 0x3ff) << 2);
-// // 	pb.val = paddr_read(pb_addr, 4);//fecth a PageTableEntry
+// 	paddr_t pb_addr = (pd.page_frame << 12) | (((vaddr >> 12) & 0x3ff) << 2);
+// 	pb.val = paddr_read(pb_addr, 4);//fecth a PageTableEntry
   
-// // 	// Log("cr3:0x%08x pd_addr:0x%08x pb_val:0x%08x pb_addr:0x%08x pb_val:0x%08x", cpu.cr3.page_directory_base, pd_addr, pd.val, pb_addr, pb.val);
-// // 	assert(pb.present == 1);	
-// //   //tell os both levels are used;replacement algorithm
-// // 	if(pd.accessed == 0){
-// // 		pd.accessed = 1;
-// // 		paddr_write(pd_addr, 4, pd.val);
-// // 	}
-// // 	if(pb.accessed == 0) {
-// // 	  pb.accessed = 1;
-// // 	  paddr_write(pb_addr, 4, pb.val);
-// // 	}
-// // 	//writeback
-// //   if(pb.dirty == 0 && pb.read_write == 1) {
-// // 		pb.dirty = 1;
-// // 		paddr_write(pb_addr, 4, pb.val);
-// // 	}
+// 	// Log("cr3:0x%08x pd_addr:0x%08x pb_val:0x%08x pb_addr:0x%08x pb_val:0x%08x", cpu.cr3.page_directory_base, pd_addr, pd.val, pb_addr, pb.val);
+// 	assert(pb.present == 1);	
+//   //tell os both levels are used;replacement algorithm
+// 	if(pd.accessed == 0){
+// 		pd.accessed = 1;
+// 		paddr_write(pd_addr, 4, pd.val);
+// 	}
+// 	if(pb.accessed == 0) {
+// 	  pb.accessed = 1;
+// 	  paddr_write(pb_addr, 4, pb.val);
+// 	}
+// 	//writeback
+//   if(pb.dirty == 0 && pb.read_write == 1) {
+// 		pb.dirty = 1;
+// 		paddr_write(pb_addr, 4, pb.val);
+// 	}
   
-// // 	paddr_t paddr = (pb.page_frame << 12) | (vaddr & 0xfff);
-// //   // Log("va:0x%08x pa:0x%08x", vaddr, paddr);
-// // 	return paddr;
+// 	paddr_t paddr = (pb.page_frame << 12) | (vaddr & 0xfff);
+//   // Log("va:0x%08x pa:0x%08x", vaddr, paddr);
+// 	return paddr;
 
-//   PDE pde, *pgdir;
-//   PTE pte, *pgtab;
-//   paddr_t paddr = addr;
-
-//   if (cpu.cr0.protect_enable && cpu.cr0.paging) {
-//     pgdir = (PDE *)(intptr_t)(cpu.cr3.page_directory_base << 12);
-//     pde.val = paddr_read((intptr_t)&pgdir[(addr >> 22) & 0x3ff], 4);
-//     Assert(pde.present, "Page dir not valid: va 0x%x eip: 0x%x", addr, cpu.eip);
-//     pde.accessed = 1;
-
-//     pgtab = (PTE *)(intptr_t)(pde.page_frame << 12);
-//     pte.val = paddr_read((intptr_t)&pgtab[(addr >> 12) & 0x3ff], 4);
-//     Assert(pte.present, "Page dir not valid: va 0x%x", addr);
-//     pte.accessed = 1;
-//     pte.dirty = pte.read_write ? 1 : pte.dirty;
-//     // Log("va:0x%08x pa:0x%08x",addr, paddr);
-   
-//     paddr = (pte.page_frame << 12) | (addr & PAGE_MASK);
-// 	// Log("va:0x%08x pa:0x%08x",addr, paddr);
-//   }
-
-//   return paddr;
-// }
-
-paddr_t page_translate(vaddr_t addr, bool is_write) {
   PDE pde, *pgdir;
   PTE pte, *pgtab;
   paddr_t paddr = addr;
@@ -92,13 +67,38 @@ paddr_t page_translate(vaddr_t addr, bool is_write) {
     pte.val = paddr_read((intptr_t)&pgtab[(addr >> 12) & 0x3ff], 4);
     Assert(pte.present, "Page dir not valid: va 0x%x", addr);
     pte.accessed = 1;
-    pte.dirty = is_write ? 1 : pte.dirty;
-
+    pte.dirty = pte.read_write ? 1 : pte.dirty;
+    // Log("va:0x%08x pa:0x%08x",addr, paddr);
+   
     paddr = (pte.page_frame << 12) | (addr & PAGE_MASK);
+	// Log("va:0x%08x pa:0x%08x",addr, paddr);
   }
 
   return paddr;
 }
+
+// paddr_t page_translate(vaddr_t addr, bool is_write) {
+//   PDE pde, *pgdir;
+//   PTE pte, *pgtab;
+//   paddr_t paddr = addr;
+
+//   if (cpu.cr0.protect_enable && cpu.cr0.paging) {
+//     pgdir = (PDE *)(intptr_t)(cpu.cr3.page_directory_base << 12);
+//     pde.val = paddr_read((intptr_t)&pgdir[(addr >> 22) & 0x3ff], 4);
+//     Assert(pde.present, "Page dir not valid: va 0x%x eip: 0x%x", addr, cpu.eip);
+//     pde.accessed = 1;
+
+//     pgtab = (PTE *)(intptr_t)(pde.page_frame << 12);
+//     pte.val = paddr_read((intptr_t)&pgtab[(addr >> 12) & 0x3ff], 4);
+//     Assert(pte.present, "Page dir not valid: va 0x%x", addr);
+//     pte.accessed = 1;
+//     pte.dirty = is_write ? 1 : pte.dirty;
+
+//     paddr = (pte.page_frame << 12) | (addr & PAGE_MASK);
+//   }
+
+//   return paddr;
+// }
 
 uint32_t paddr_read(paddr_t addr, int len) {
   int mmio_id = is_mmio(addr);
