ae1e0e2f3d8ee8891c78b076cdc4c741c0cc0cba
tracer-ics2017 <tracer@njuics.org>
2019-06-17 23:59:28 +0800

    >  compile 161730319 wuyangjun Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  23:59:28 up  2:14,  5 users,  load average: 0.03, 0.57, 0.87 3e0132ebe59628cc292106227d0cea4dd36326ae

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -155,7 +155,17 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
 uint32_t vaddr_read(vaddr_t addr, int len) {
 	if(cpu.cr0.protect_enable && cpu.cr0.paging){
 		if( CROSS_PAGE(addr, len)) {//cross page read 
-			assert(0);
+			// assert(0);
+      paddr_t paddr;
+      union {
+      uint8_t bytes[4];
+      uint32_t dword;
+    } data = {0};
+    for (int i = 0; i < len; i++) {
+      paddr = page_translate(addr + i);
+      data.bytes[i] = (uint8_t)paddr_read(paddr, 1);
+    }
+    return data.dword;
 		}
 		else {
 			paddr_t paddr = page_translate(addr);
