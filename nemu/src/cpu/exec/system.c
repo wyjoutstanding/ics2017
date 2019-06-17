f95de801d8df6f88810effc16b2af2a9cdb7a800
tracer-ics2017 <tracer@njuics.org>
2019-06-17 23:09:49 +0800

    >  compile 161730319 wuyangjun Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  23:09:49 up  1:24,  5 users,  load average: 0.13, 0.03, 0.07 a62973cae9d19f24908ae89af54d15eb1d37a5a

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -1,48 +1,60 @@
 #include "cpu/exec.h"
-#include "memory/mmu.h"
 
-extern void raise_intr(uint8_t NO, vaddr_t ret_addr);
 void diff_test_skip_qemu();
 void diff_test_skip_nemu();
 
 make_EHelper(lidt) {
-  cpu.IDTR.limit = vaddr_read(id_dest->addr, 2);
-  if (decoding.is_operand_size_16)
-    cpu.IDTR.base = vaddr_read(id_dest->addr + 2, 3);
-  else
-    cpu.IDTR.base = vaddr_read(id_dest->addr + 2, 4);
-
+//  TODO();
+//  rtl_lm((rtlreg_t*)&cpu.IDTR.limit,id_src->addr,2);
+	cpu.IDTR.limit = vaddr_read(id_dest->addr,2);
+//	Log("limit:%x\n",cpu.IDTR.limit);
+	if(decoding.is_operand_size_16){
+		t3 = vaddr_read(id_dest->addr+2,4);
+		t3 = t3 & 0x00ffffff;
+		rtl_mv((rtlreg_t*)&cpu.IDTR.base,&t3);
+	}
+	else {
+		cpu.IDTR.base = vaddr_read(id_dest->addr+2,4);
+	}
+//	Log("base:%x\n",cpu.IDTR.base);
+	 //	rtl_mv((rtlreg_t*)&cpu.IDTR.base,(rtlreg_t*)&(vaddr_read(id_src->addr+2,4)));
   print_asm_template1(lidt);
 }
 
 make_EHelper(mov_r2cr) {
-  switch (id_dest->reg) {
-  case 0: cpu.cr0.val = id_src->val; break;
-  case 3: cpu.cr3.val = id_src->val; break;
-  default: Assert(0, "unsupported control register");
-  }
+//  TODO();
+   if(id_dest->reg == 0) cpu.cr0.val = reg_l(id_src->reg);
+   else cpu.cr3.val = reg_l(id_src->reg);
 
+  // switch (id_dest->reg) {
+	//   case 0: cpu.cr0.val = id_src->val; break;
+	//   case 3: cpu.cr3.val = id_src->val; break;
+	//   default: Assert(0, "unsupported control register");
+  // }
   print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
 make_EHelper(mov_cr2r) {
-  switch (id_src->reg) {
-  case 0: t0 = cpu.cr0.val; break;
-  case 3: t0 = cpu.cr3.val; break;
-  default: Assert(0, "unsupported control register");
-  }
-  operand_write(id_dest, &t0);
+//  TODO();
+  if(id_src->reg == 0) reg_l(id_dest->reg) = cpu.cr0.val;
+  else reg_l(id_dest->reg) = cpu.cr3.val;
 
+  // switch (id_src->reg) {
+	//   case 0: t0 = cpu.cr0.val; break;
+	//   case 3: t0 = cpu.cr3.val; break;
+	//   default: Assert(0, "unsupported control register");
+  // }
+  // operand_write(id_dest, &t0);
   print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
 
 #ifdef DIFF_TEST
   diff_test_skip_qemu();
 #endif
 }
-
+void raise_intr(uint8_t, vaddr_t);
 make_EHelper(int) {
-  raise_intr(id_dest->val, decoding.seq_eip);
-
+//  TODO();
+	raise_intr((uint8_t)id_dest->val,decoding.seq_eip); 	
   print_asm("int %s", id_dest->str);
 
 #ifdef DIFF_TEST
@@ -51,14 +63,11 @@ make_EHelper(int) {
 }
 
 make_EHelper(iret) {
-  rtl_pop(&t0);
-  decoding.is_jmp = 1;
-  decoding.jmp_eip = t0;
-  rtl_pop(&t0);
-  cpu.CS = (uint16_t)t0;
-  rtl_pop(&t0);
-  cpu.EFLAGS.val = t0;
-
+//  TODO();
+  rtl_pop(&decoding.jmp_eip);
+	decoding.is_jmp = true;
+	rtl_pop(&cpu.CS);
+	rtl_pop((rtlreg_t*)&cpu.EFLAGS);
   print_asm("iret");
 }
 
@@ -66,9 +75,9 @@ uint32_t pio_read(ioaddr_t, int);
 void pio_write(ioaddr_t, int, uint32_t);
 
 make_EHelper(in) {
-  t1 = pio_read(id_src->val, id_dest->width);
-  operand_write(id_dest, &t1);
-
+ // TODO();
+  uint32_t data = pio_read(id_src->val,id_src->width);
+	operand_write(id_dest,&data);
   print_asm_template2(in);
 
 #ifdef DIFF_TEST
@@ -77,8 +86,9 @@ make_EHelper(in) {
 }
 
 make_EHelper(out) {
-  pio_write(id_dest->val, id_dest->width, id_src->val);
-
+//  TODO();
+  pio_write(id_dest->val,id_src->width,id_src->val);
+	diff_test_skip_qemu();
   print_asm_template2(out);
 
 #ifdef DIFF_TEST
