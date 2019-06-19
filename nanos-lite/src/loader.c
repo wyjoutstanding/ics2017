9d9e3ed9fbee1a677f2fd762650f31eed7b26f4f
tracer-ics2017 <tracer@njuics.org>
2019-06-19 18:53:46 +0800

    >  run 161730319 wuyangjun Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  18:53:46 up 43 min,  2 users,  load average: 0.00, 0.00, 0.04 c56cbc3295ee99dd7ca21b3086ff73095922c8c

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -11,46 +11,45 @@ int fs_close(int fd);
 size_t fs_filesz(int fd);
 void* new_page(void);
 
-// uintptr_t loader(_Protect *as, const char *filename) {
-// //  TODO();
-// //  ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());
-// /*	int fd = fs_open(filename, 0, 0);
-//   fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd));
-// 	fs_close(fd);
-// */  
-//   Log("start");
-// 	int fd = fs_open(filename, 0, 0);
-// 	uint32_t file_size = fs_filesz(fd);
-// 	int nr_page = 0;
-// 	void* pa, *va = as->ptr;
-// 	while(nr_page * PAGE_SIZE < file_size) {
-// 		pa = new_page();
-// 		assert(pa != NULL);
-// 		nr_page++;
-		
-//     Log("Map va to pa: 0x%08x to 0x%08x", va, pa);
-// 		_map(as, va, pa);
-// 	  va += PAGE_SIZE;
-// 		fs_read(fd, pa, PAGE_SIZE);
-// 	}
-// 	fs_close(fd);
-// 	return (uintptr_t)DEFAULT_ENTRY;
-// }
 uintptr_t loader(_Protect *as, const char *filename) {
-  int fd = fs_open(filename, 0, 0);
-  size_t nbyte = fs_filesz(fd);
-  void *pa;
-  void *va;
-
-  Log("loaded: [%d]%s size:%d", fd, filename, nbyte);
-
-  void *end = DEFAULT_ENTRY + nbyte;
-  for (va = DEFAULT_ENTRY; va < end; va += PGSIZE) {
-    pa = new_page();
-    _map(as, va, pa);
-    Log("map: va 0x%x pa 0x%x", va, pa);
-    fs_read(fd, pa, (end - va) < PGSIZE ? (end - va) : PGSIZE);
-  }
-
-  return (uintptr_t)DEFAULT_ENTRY;
+//  TODO();
+//  ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());
+/*	int fd = fs_open(filename, 0, 0);
+  fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd));
+	fs_close(fd);
+*/  
+  Log("start");
+	int fd = fs_open(filename, 0, 0);
+	uint32_t file_size = fs_filesz(fd);
+	int nr_page = 0;
+	void *pa, *va = DEFAULT_ENTRY;
+	while(nr_page * PAGE_SIZE < file_size) {
+		pa = new_page();
+		assert(pa != NULL);
+		nr_page++;
+    Log("Map va to pa: 0x%08x to 0x%08x", va, pa);
+		_map(as, va, pa);
+	  va += PAGE_SIZE;
+		fs_read(fd, pa, PAGE_SIZE);
+	}
+	fs_close(fd);
+	return (uintptr_t)DEFAULT_ENTRY;
 }
+// uintptr_t loader(_Protect *as, const char *filename) {
+//   int fd = fs_open(filename, 0, 0);
+//   size_t nbyte = fs_filesz(fd);
+//   void *pa;
+//   void *va;
+
+//   Log("loaded: [%d]%s size:%d", fd, filename, nbyte);
+
+//   void *end = DEFAULT_ENTRY + nbyte;
+//   for (va = DEFAULT_ENTRY; va < end; va += PGSIZE) {
+//     pa = new_page();
+//     _map(as, va, pa);
+//     Log("map: va 0x%x pa 0x%x", va, pa);
+//     fs_read(fd, pa, (end - va) < PGSIZE ? (end - va) : PGSIZE);
+//   }
+
+//   return (uintptr_t)DEFAULT_ENTRY;
+// }
