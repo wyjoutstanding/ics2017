a4738a71645d82eba77305fc07fc3384dfbd7265
tracer-ics2017 <tracer@njuics.org>
2019-06-23 14:55:23 +0800

    >  compile 161730319 wuyangjun Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  14:55:23 up  5:18,  3 users,  load average: 0.00, 0.00, 0.00 b3e7d12fb0c00aeb29b96f87a69e3417dd62e87c

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -255,14 +255,15 @@ void exec_wrapper(bool print_flag) {
 #ifdef DIFF_TEST
   uint32_t eip = cpu.eip;
 #endif
+  update_eip();
   void raise_intr(uint8_t NO, vaddr_t ret_addr);
   //判断是否进行中断处理
   if(cpu.INTR && cpu.EFLAGS.IF){
     cpu.INTR = false;
     raise_intr(TIMER_IRQ, cpu.eip);
+    update_eip();
   }
 
-  update_eip();
 
 #ifdef DIFF_TEST
 //	printf("diff eip:%08x\n",eip);
